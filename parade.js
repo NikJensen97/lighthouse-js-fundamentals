3c6973d291cfda1783dec9aedad1fa9f91ec1612
NikJensen97 <npjjensen@hotmail.com>
2023-02-16 20:34:33 -0800

    function returns X and Y coordinates from a given array

---
+++ parade.js
@@ -0,0 +1,20 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north']
+
+
+const finalPosition = function (moves) {
+  var position = [0, 0];
+  for(const move of moves){
+    if(move === 'north'){
+      position[1] += 1;
+    }
+    else if(move === 'south'){
+      position[1] -= 1;
+    }
+    else if(move === 'east'){
+      position[0] += 1;
+    }
+    else position[0] -= 1;
+  }
+  return position;
+}
+finalPosition(moves);
