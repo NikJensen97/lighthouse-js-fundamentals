070c23021b6f951be6b013610112460c16994b1b
NikJensen97 <npjjensen@hotmail.com>
2023-02-18 22:22:38 -0800

    function that returns the last position of a given value in an array

---
+++ lastIndex.js
@@ -0,0 +1,14 @@
+function lastIndexOf(array, index){
+  var place = -1;
+  for(let i = 0; i < array.length; i++){
+    if(array[i] == index){
+      place = i;
+    }
+    
+  }
+  
+  
+   
+  
+  return place;
+}
